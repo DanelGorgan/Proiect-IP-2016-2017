f258616de043a489ae97a9f69da5b742dbda3b89
GorganGabi <gabrielgorgan23@yahoo.com>
2017-01-03 22:18:51 +0200

    Coada adaugata
    Am adaugat coada sarpelui care creste atunci cand mananca un fruct. Ptr
    acest lucru, am modificat functia Draw(), Logic(). De asemenea, am facut
    un mic meniu la inceput.

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-bool gameover;
+bool gameOver;
 int width;
 int height;
 int score;
@@ -14,13 +14,19 @@ eDirection dir;
 struct Snake
 {
     int x, y;
-}snake;
+} snake;
 
 struct Fruit
 {
     int x, y;
-}fruit;
+} fruit;
 
+struct Tail
+{
+    int x[100];
+    int y[100];
+    int lenght;
+} tail;
 void InitGame()
 {
     width=30;
@@ -37,7 +43,7 @@ void Draw()
 
     system("cls");
 
-    for (i=0; i<width+1; i++)
+    for (i=0; i<width+2; i++)
         cout<<"#";
     cout<<endl;
 
@@ -47,18 +53,30 @@ void Draw()
         {
             if(j==0)
                 cout<<"#";
-            else if (i==snake.y && j==snake.x)
+            if (i==snake.y && j==snake.x)
                 cout<<"O";
             else if (i==fruit.y && j==fruit.x)
                 cout<<"F";
-            else cout<<" ";
+            else
+            {
+                bool ok=false;
+                for (int k=0; k<tail.lenght; k++)
+                {
+                    if (tail.x[k]==j && tail.y[k]==i)
+                    {
+                        cout<<"o";
+                        ok=true;
+                    }
+                }
+                if (ok==false) cout<<" ";
+            }
             if (j==width-1)
                 cout<<"#";
         }
         cout<<endl;
     }
 
-    for (i=0; i<width+1; i++)
+    for (i=0; i<width+2; i++)
         cout<<"#";
     cout<<endl;
     cout<<"Score: "<<score;
@@ -77,30 +95,35 @@ void Input()
             dir=RIGHT;
             break;
             if(dir!=UP)
-        case 's':
+            case 's':
             dir=DOWN;
             break;
         case 'w':
             dir=UP;
             break;
         case 'x':
-            gameover=true;
+            gameOver=true;
             break;
         }
-    }
-    if(snake.x>width-1 || snake.x<1 || snake.y>height-1 || snake.y <0)
-        gameover=true;
-
-    if(snake.x==fruit.x && snake.y==fruit.y)
-    {
-        score+=10;
-        fruit.x=rand()%width;
-        fruit.y=rand()%height;
     }
 }
 
 void Logic()
 {
+    int prevX=tail.x[0];
+    int prevY=tail.y[0];
+    int prevX1, prevY1;
+    tail.x[0]=snake.x;
+    tail.y[0]=snake.y;
+    for (int i=1; i<tail.lenght; i++)
+    {
+        prevX1=tail.x[i];
+        prevY1=tail.y[i];
+        tail.x[i]=prevX;
+        tail.y[i]=prevY;
+        prevY=prevY1;
+        prevX=prevX1;
+    }
 
     switch(dir)
     {
@@ -117,21 +140,37 @@ void Logic()
         snake.y++;
         break;
     }
+    if(snake.x>width-1 || snake.x<1 || snake.y>height-1 || snake.y <0)
+        gameOver=true;
+
+    for (int i=0; i<tail.lenght; i++)
+        if (tail.x[i]==snake.x && tail.y[i]==snake.y)
+            gameOver=1;
+
+    if(snake.x==fruit.x && snake.y==fruit.y)
+    {
+        tail.lenght++;
+        score+=10;
+        fruit.x=rand()%width;
+        fruit.y=rand()%height;
+    }
 }
 
 int main()
 {
+    cout<<"***********************************SNAKE GAME***********************************";
+    cout<<"                                                                    by TwinsG"<<endl;
+    cout<<"                              PRESS ANYTHING TO START                           ";
     InitGame();
-    while(!gameover)
-    {
-        Draw();
-        Input();
-        Logic();
-        Sleep(75);
-    }
+    if (_getch())
+        while(!gameOver)
+        {
+            Draw();
+            Input();
+            Logic();
+            Sleep(75);
+        }
     system("cls");
     cout<<"GAME OVER!";
 
 }
-
-
