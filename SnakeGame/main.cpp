8b9389ad5f61c68975f0dce9b85fbb0d1f7e51d1
danelgorgan <danielgorgan24@yahoo.com>
2017-01-03 20:09:16 +0200

    Miscare sarpe pe harta
    Am creat o functie Input() care are rolul de a prelua input-ul de la
    tastatura, dupa care am facut functia Logic() care muta sarpele in
    directia primita din Input(). Am updatat functia de main prin care
    miscarea este de fapt desenarea hartii incontinuu, prin mutarea pozitiei
    sarpelui.Am adaugat si scor. Am folosit functia Sleep() pentru a
    incetini jocul.

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <windows.h>
+#include<conio.h>
 
 using namespace std;
 
 bool gameover;
 int width;
 int height;
+int score;
+enum eDirection {STOP=0, LEFT,RIGHT,UP,DOWN};
+eDirection dir;
 
 struct Snake
 {
@@ -20,7 +24,7 @@ struct Fruit
 void InitGame()
 {
     width=30;
-    height=20;
+    height=18;
     snake.x= width /2 ;
     snake.y= height / 2;
     fruit.x=rand()%width;
@@ -33,7 +37,7 @@ void Draw()
 
     system("cls");
 
-    for (i=0; i<width; i++)
+    for (i=0; i<width+1; i++)
         cout<<"#";
     cout<<endl;
 
@@ -41,25 +45,93 @@ void Draw()
     {
         for(j=0; j<width; j++)
         {
-            if(j==0 || j==width-1)
+            if(j==0)
                 cout<<"#";
-            else if (i==snake.x && j==snake.y)
+            else if (i==snake.y && j==snake.x)
                 cout<<"O";
-            else if (i==fruit.x && j==fruit.y)
+            else if (i==fruit.y && j==fruit.x)
                 cout<<"F";
             else cout<<" ";
+            if (j==width-1)
+                cout<<"#";
         }
         cout<<endl;
     }
-    for (i=0; i<width; i++)
+
+    for (i=0; i<width+1; i++)
         cout<<"#";
     cout<<endl;
+    cout<<"Score: "<<score;
+}
+
+void Input()
+{
+    if(_kbhit())
+    {
+        switch(_getch())
+        {
+        case 'a':
+            dir=LEFT;
+            break;
+        case 'd':
+            dir=RIGHT;
+            break;
+            if(dir!=UP)
+        case 's':
+            dir=DOWN;
+            break;
+        case 'w':
+            dir=UP;
+            break;
+        case 'x':
+            gameover=true;
+            break;
+        }
+    }
+    if(snake.x>width-1 || snake.x<1 || snake.y>height-1 || snake.y <0)
+        gameover=true;
+
+    if(snake.x==fruit.x && snake.y==fruit.y)
+    {
+        score+=10;
+        fruit.x=rand()%width;
+        fruit.y=rand()%height;
+    }
+}
+
+void Logic()
+{
+
+    switch(dir)
+    {
+    case LEFT:
+        snake.x--;
+        break;
+    case RIGHT:
+        snake.x++;
+        break;
+    case UP:
+        snake.y--;
+        break;
+    case DOWN:
+        snake.y++;
+        break;
+    }
 }
 
 int main()
 {
     InitGame();
-    Draw();
+    while(!gameover)
+    {
+        Draw();
+        Input();
+        Logic();
+        Sleep(75);
+    }
+    system("cls");
+    cout<<"GAME OVER!";
+
 }
 
 
